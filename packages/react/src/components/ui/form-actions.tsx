4f69e5da9b8954f66e558176f61cd831ef78adf3
naveen-s-chand_atko <naveen.s.chand@okta.com>
2025-09-04 10:41:19 +0530

    fix(react): use classname in form actions

--- packages/react/src/components/ui/form-actions.tsx
+++ packages/react/src/components/ui/form-actions.tsx
@@ -90,6 +90,7 @@ export const FormActions: React.FC<FormActionsProps> = ({
           size={previousButtonProps.size}
           onClick={handlePreviousClick}
           disabled={previousButtonProps.disabled || isLoading}
+          className={previousButtonProps.className}
         >
           {previousButtonProps.label}
         </Button>
@@ -100,6 +101,7 @@ export const FormActions: React.FC<FormActionsProps> = ({
         variant={nextButtonProps.variant}
         size={nextButtonProps.size}
         disabled={nextButtonProps.disabled || isLoading}
+        className={nextButtonProps.className}
         onClick={handleNextClick}
       >
         {isLoading ? <Spinner size="sm" aria-hidden="true" /> : nextButtonProps.label}
