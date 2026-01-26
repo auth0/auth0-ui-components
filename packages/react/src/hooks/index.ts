f6ff813b6480fd1c18e769b3828ab13bf895265c
Pablo Castro Valiño <pablo.castro@okta.com>
2026-01-26 14:15:40 +0100

    feat: export missing hooks

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,6 +1,7 @@
 export { useCoreClient, CoreClientContext } from './use-core-client';
 export { useTranslator } from './use-translator';
 export { useTheme } from './use-theme';
+export { useToastProvider } from './use-toast-provider';
 export { useCoreClientInitialization } from './use-core-client-initialization';
 export { useScopeManager } from './use-scope-manager';
 export { useErrorHandler } from './use-error-handler';
@@ -11,3 +12,10 @@ export {
   useMFA,
 } from './my-account/mfa';
 export { useOrganizationDetailsEdit } from './my-organization/organization-management/use-organization-details-edit';
+export { useDomainTable } from './my-organization/domain-management/use-domain-table';
+export { useDomainTableLogic } from './my-organization/domain-management/use-domain-table-logic';
+export { useProviderFormMode } from './my-organization/idp-management/use-provider-form-mode';
+export { useSsoDomainTab } from './my-organization/idp-management/use-sso-domain-tab';
+export { useSsoProviderCreate } from './my-organization/idp-management/use-sso-provider-create';
+export { useSsoProviderEdit } from './my-organization/idp-management/use-sso-provider-edit';
+export { useSsoProviderTable } from './my-organization/idp-management/use-sso-provider-table';
