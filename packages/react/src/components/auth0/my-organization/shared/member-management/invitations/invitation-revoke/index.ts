95de155ef5ad9aad100c09923c3176089ee2a6c6
rax7389 <rahul.bhowmick@okta.com>
2026-03-10 23:45:57 +0530

    chore: removed unwanted files

--- packages/react/src/components/auth0/my-organization/shared/member-management/invitations/invitation-revoke/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Invitation revoke components.
- * @module invitation-revoke
- */
-
-export { OrganizationInvitationRevokeModal } from './organization-invitation-revoke-modal';
-export type { OrganizationInvitationRevokeModalProps } from './organization-invitation-revoke-modal';
--- packages/react/src/components/auth0/my-organization/shared/member-management/invitations/invitation-table/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Invitation table components.
- * @module invitation-table
- */
-
-export { OrganizationInvitationTable } from './organization-invitation-table';
-export { InvitationSearchFilter } from './invitation-search-filter';
-export { OrganizationInvitationTableActionsColumn } from './organization-invitation-table-actions-column';
