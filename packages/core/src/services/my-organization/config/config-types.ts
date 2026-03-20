392671379ebd015e36f4127515fa79010f80d7e9
rax7389 <rahul12bhowmick@gmail.com>
2026-03-20 21:07:11 +0530

    chore: removed unused type

--- packages/core/src/services/my-organization/config/config-types.ts
+++ packages/core/src/services/my-organization/config/config-types.ts
@@ -8,7 +8,4 @@ import type { MyOrganization } from '@auth0/myorganization-js';
 export type GetConfigurationResponseContent = MyOrganization.GetConfigurationResponseContent;
 
 export type GetIdpConfigurationResponseContent = MyOrganization.GetIdpConfigurationResponseContent;
-export type IdpConfigEnabledFeatures = MyOrganization.IdentityProvidersConfigEnabledFeaturesEnum;
-export type IdpConfigProvisioningMethods =
-  MyOrganization.IdentityProvidersConfigProvisioningMethodsEnum;
 export type IdpConfigStrategyBase = MyOrganization.IdentityProvidersConfigStrategyBase;
